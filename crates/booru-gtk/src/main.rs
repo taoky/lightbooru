d4cd82eb1f6b22b3b4c7c35cf984f9c289f37432
taoky <me@taoky.moe>
2026-02-20 21:11:50 +0800

    Set default log level to info

--- crates/booru-gtk/src/main.rs
+++ crates/booru-gtk/src/main.rs
@@ -56,7 +56,7 @@ fn main() -> Result<()> {
 
 fn init_tracing() {
     let filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("booru_gtk=debug"));
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("booru_gtk=info"));
     let _ = tracing_subscriber::fmt()
         .with_env_filter(filter)
         .with_target(false)
